abf5164796243c68e329e0fb112594a6ec55c428
juan_ <reyes.c.juan.p@gmail.com>
2022-04-01 13:12:56 -0600

    Comprobacion de DATABINDING MEDIANNTE UN BOTON

--- app/src/main/java/com/example/trivia17ods/MainActivity.kt
+++ app/src/main/java/com/example/trivia17ods/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.trivia17ods
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.trivia17ods.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var databinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        databinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        databinding.button1.setOnClickListener {
+           databinding.textprincipal.text="databinding"
+        }
     }
 }
